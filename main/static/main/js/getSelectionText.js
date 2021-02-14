bee77093d92bc788f84610ddfd47572cdaad883d
aviadm24 <aviadm24@gmail.com>
2021-02-14 21:03:51 +0200

    added highlight

--- main/static/main/js/getSelectionText.js
+++ main/static/main/js/getSelectionText.js
@@ -1,6 +1,36 @@
-function getSelectedText() {
 
+function highlight(comment_reference, index) { // https://stackoverflow.com/questions/52743841/find-and-highlight-word-in-text-using-js
+  var paragraph = document.getElementById('text');
+  var opar = paragraph.innerHTML;
+  //var search = document.getElementById('typed-text').value;
+  search = comment_reference.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); //https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+
+  var re = new RegExp(search, 'g');
+  var m;
+  var matches = opar.matchAll(re)
+  for (const match of matches) {
+    console.log(`Found ${match[0]} start=${match.index} end=${match.index + match[0].length}.`);
+      if (index == match.index) {
+       innerHTML = opar.substring(0,index) + "<span class='highlight'>" + opar.substring(index,index+text.length) + "</span>" + opar.substring(index + text.length);
+       paragraph.innerHTML = innerHTML;
+      }
+    }
+
+//  if (search.length > 0)
+//    paragraph.innerHTML = opar.replace(re, `<mark>$&</mark>`);
+//  else paragraph.innerHTML = opar;
 }
+
+$( document ).ready(function() {
+  $(".comment_reference").each(function(){
+    comment_reference = $(this).text();
+    console.log("comment reference: "+comment_reference);
+    text = comment_reference.split('/')[0];
+    index = comment_reference.split('/')[1];
+    highlight(text, index);
+  })
+});
+
 
 $("#text").mouseup(function(){
     var selectedText = '';
@@ -20,6 +50,14 @@ $("#text").mouseup(function(){
     } else return;
     // To write the selected text into the textarea
    console.log('selected text: '+ selectedText);
-   $('#id_comment_reference').val(selectedText);
-
+   var thisText = $(this).text();
+   var start = thisText.indexOf(selectedText);
+   console.log("length: " + selectedText.length);
+   var end = start + selectedText.length;
+   //if (start >= 0 && end >= 0){
+        console.log("start: " + start);
+        console.log("end: " + end);
+    //}
+   $('#id_comment_reference').val(selectedText+'/'+start.toString());
+  // https://stackoverflow.com/questions/9756941/knowing-the-text-selected-using-mouse-in-javascript
 });
--- main/static/main/js/search.js
+++ main/static/main/js/search.js
@@ -15,11 +15,14 @@ $('#search_form').on('click', function (e) {
             $('#search_results').empty();
             $.each(data.hits.hits, function() {
                 var text = this.highlight.exact;
-                console.log(this._id);
+
 //                var regExp = /\(([0-9:+]+)\)/;
 //                var matches = regExp.exec(this._id);
-                var p = this._id.split("(")
-                var output='<li>'+text+'</li>'+'<a href='+p[0]+'>'+p[1]+'</a>';
+//                var p = this._id.split("(");
+                var p = this._id.replace(/ *\([^)]*\) */g, "");
+                var link = '/api/texts/'+p // '/'+$(location).attr('host')+
+                console.log(link);
+                var output='<li>'+text+'</li><a href="'+link+'">'+p+'</a>';
                 $('#search_results').append(output);
             });
         },
