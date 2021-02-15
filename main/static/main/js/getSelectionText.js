08f90f3fbad0b27abff1d08da64f54ecfbfff766
aviadm24 <aviadm24@gmail.com>
2021-02-16 01:12:02 +0200

    trying to get offset

--- main/static/main/js/getSelectionText.js
+++ main/static/main/js/getSelectionText.js
@@ -9,6 +9,7 @@ function highlight(comment_reference, index) { // https://stackoverflow.com/ques
   var m;
   var matches = opar.matchAll(re)
   for (const match of matches) {
+  console.log("offset: "+ match.off)
     console.log(`Found ${match[0]} start=${match.index} end=${match.index + match[0].length}.`);
       if (index == match.index) {
        innerHTML = opar.substring(0,index) + "<span class='highlight'>" + opar.substring(index,index+text.length) + "</span>" + opar.substring(index + text.length);
@@ -20,6 +21,22 @@ function highlight(comment_reference, index) { // https://stackoverflow.com/ques
 //    paragraph.innerHTML = opar.replace(re, `<mark>$&</mark>`);
 //  else paragraph.innerHTML = opar;
 }
+// https://stackoverflow.com/questions/51277123/get-character-offsets-of-beginning-and-end-of-selected-text
+document.addEventListener("mouseup",function(){
+    if(window.getSelection)
+    {
+        var selectedtext = window.getSelection().toString();
+        var range = window.getSelection().getRangeAt(0);
+        var content = range.startContainer.textContent;
+        content = content.replace(/(?:\r\n|\r|\n)/g, '');
+        if(range.startContainer.parentElement.tagName=="BODY")
+        {
+            content = content.replace(/^\s*|\s*$/, '');
+        }
+        console.log(content, content.indexOf(selectedtext), content.indexOf(selectedtext)+selectedtext.length)
+    }
+},false);
+
 
 $( document ).ready(function() {
   $(".comment_reference").each(function(){
@@ -50,6 +67,9 @@ $("#text").mouseup(function(){
     } else return;
     // To write the selected text into the textarea
    console.log('selected text: '+ selectedText);
+   console.log(selectedText.focusNode.data[selectedText.focusOffset]);
+  alert(selectedText.focusOffset);
+   //$('#commentModal').modal('show');
    var thisText = $(this).text();
    var start = thisText.indexOf(selectedText);
    console.log("length: " + selectedText.length);
